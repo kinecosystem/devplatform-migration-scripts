2f63bd92c38b906201c0a356cba71962adb17c6b
ronserruya <ron.serruya@kik.com>
2019-01-27 15:33:48 +0200

    add cold wallet script

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # devplatform-migration-scripts
 Scripts for the developer platform participants for the migration
+
+## Requires:
+* Python >= 3.4
+
+## Usage:
+
+Install the python pacakages
+```bash
+pip install --user requests
+pip install --user kin-sdk==2.3.0
+```
+
+and run the script
+```
+python cold_wallet.py
+```
+Now follow the prompts from the script
+
+Example:
+```
+Migration script for cold wallet
+Choose environment:
+1.Production
+2.Testnet
+[1/2]: 2
+Initiated on testnet
+Input the seed of your wallet: SBSROJDBNA2P4US7E4CMQBY2OMN65XMBCU6NN545V7SQ4BN5GEJARJ64
+Burn succeeded
+Done
+```
---
+++ cold_wallet.py
@@ -0,0 +1,77 @@
+import kin
+import requests
+
+# New sdk doesn't have the old blockchain urls
+
+
+def get_kin_balance(client, address, issuer):
+    # New sdk only checks for native balance
+    data = client.get_account_data(address)
+    kin_balance = None
+    for balance in data.balances:
+        if balance['asset_code'] == 'KIN' and balance['asset_issuer'] == issuer:
+            kin_balance = balance['balance']
+
+    if kin_balance is None:
+        print('No kin balance found')
+        quit(1)
+
+    return kin_balance
+
+
+prod = kin.Environment('PROD', 'https://horizon-ecosystem.kininfrastructure.com',
+                       'Public Global Kin Ecosystem Network ; June 2018')
+test = kin.Environment('TEST', 'https://horizon-playground.kininfrastructure.com',
+                       'Kin Playground Network ; June 2018')
+
+
+print('Migration script for cold wallet')
+answer = input('Choose environment:\n'
+               '1.Production\n'
+               '2.Testnet\n'
+               '[1/2]: ')
+
+if answer == '1':
+    client = kin.KinClient(prod)
+    migration_endpoint = 'https://migration-devplatform-production.developers.kinecosystem.com'
+    issuer = 'GDF42M3IPERQCBLWFEZKQRK77JQ65SCKTU3CW36HZVCX7XX5A5QXZIVK'
+    print('Initiated on production')
+elif answer == '2':
+    client = kin.KinClient(test)
+    migration_endpoint = 'https://migration-devplatform-playground.developers.kinecosystem.com'
+    issuer = 'GBC3SG6NGTSZ2OMH3FFGB7UVRQWILW367U4GSOOF4TFSZONV42UJXUH7'
+    print('Initiated on testnet')
+else:
+    print('Invalid response')
+    quit(1)
+
+seed = input('Input the seed of your wallet: ')
+try:
+    keypair = kin.Keypair(seed)
+except kin.KinErrors.StellarSecretInvalidError:
+    print('Seed is invalid')
+    quit(1)
+
+account = client.kin_account(seed)
+balance = get_kin_balance(client, account.get_public_address(), issuer)
+builder = account.get_transaction_builder(100)
+builder.update_sequence()
+# Balance * 100 cause of the decimal points changes
+builder.append_change_trust_op('KIN',issuer,str(balance*100))
+builder.append_set_options_op(master_weight=0)
+builder.sign()
+try:
+    tx_hash = account.submit_transaction(builder)
+    print('Burn succeeded')
+except Exception as e:
+    print('Failed', e)
+
+response = requests.post(migration_endpoint+'/migrate?address=' + account.get_public_address())
+if response.ok:
+    print('Done')
+
+else:
+    print('Failed migration')
+    print(response.text)
+
+
